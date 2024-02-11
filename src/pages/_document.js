3707422ea3e6247b9577d8b7bc12f7c6774d3a63
Deepak <deepakkomma@gmail.com>
2024-02-12 03:07:50 +0530

    init

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Hela Mylavarapu</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Hela Mylavarapu's personal website" />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,11 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        Honk: ["Honk", "system-ui"],
+        Calligraffitti: ["Calligraffitti", "sans-serif"],
+        Roboto: ["Roboto", "sans-serif"],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
